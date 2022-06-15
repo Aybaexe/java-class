067738cd45bbb2c7d1abeaaa0676cab524434c85
Aibek Imanov <aybaexe@gmail.com>
2022-06-15 16:22:33 -0400

    Hello World in NetBeans

---
+++ classwork/HelloWord/src/main/java/Hello.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author Aibek Imanov
+ * *email: aybaexe@gmail.com    
+ *date: 2022/06/15
+ *purpose: My first Hello World program in an IDE
+ */
+// a005_june2022 com.mthree
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
